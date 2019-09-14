ca54b1d629eaed5dda685c552f86295799456b2a
EC2 Default User <ec2-user@ip-172-31-23-227.ap-southeast-1.compute.internal>
2019-09-14 00:52:25 +0000

    Finsh user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/signup' => 'users#new'
+  root 'static_pages#home'
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
-  root 'static_pages#home'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  resources :users
 end
